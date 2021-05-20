1317a2b55ce2ba3f1e7d779c3122f4014eb26b6a
sas10x <kristanraearcino@gmail.com>
2021-05-20 17:10:59 +0800

    flatlist

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native'
+import { Text, StyleSheet, View } from 'react-native'
 
 const ComponentsScreen = function () {
-    return <Text styles={styles.textStyle}>
-        This is the components screen
-    </Text>;
+    const name = "Kristan";
+    return (
+        <View>
+            <Text style={styles.textStyle}>Getting started with React Native!</Text> 
+            <Text style={styles.textStyle2}>My name is {name}</Text>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
     textStyle: {
-        fontSize: 30
+        fontSize: 45
+    },
+    textStyle2: {
+        fontSize: 20
     }
 })
 
---
+++ src/screens/ListScreen.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text, StyleSheet, FlatList }   from 'react-native';
+
+// const ComponentsScreen = function () {
+const ListScreen = () => {
+    const friends = [
+        {name: 'Friend1'},
+        {name: 'Friend2'},
+        {name: 'Friend3'},
+        {name: 'Friend4'},
+        {name: 'Friend5'},
+        {name: 'Friend6'}
+    ]
+    return <FlatList data={friends} renderItem={({item}) => {
+        return <Text>{item.name}</Text>
+    // return <FlatList data={friends} renderItem={(element) => {
+        // element === { item: { name: 'Friend1' }, index:0 }
+    }} />
+};
+
+const styles = StyleSheet.create({
+
+})
+
+export default ListScreen;
